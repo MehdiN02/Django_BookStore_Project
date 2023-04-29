cedeb39f2f8a5a6586625419c4ea94b16f9e1c53
MehdiN02 <130139149+MehdiN02@users.noreply.github.com>
2023-04-29 10:15:07 +0300

    Update views.py

--- books/views.py
+++ books/views.py
@@ -8,7 +8,7 @@ from .forms import NewBookForm
 
 class BookListView(generic.ListView):
     model = Book
-    paginate_by = 4  # baraye kozashtan 4 ketab dar har safhe
+    paginate_by = 4
     template_name = 'books/book_list.html'
     context_object_name = 'books'
 
