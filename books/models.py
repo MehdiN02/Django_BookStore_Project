fd9102c37d5b04fe4d340482f5ba62327ba2290f
MehdiN02 <130139149+MehdiN02@users.noreply.github.com>
2023-04-29 10:14:23 +0300

    Update models.py

--- books/models.py
+++ books/models.py
@@ -6,19 +6,13 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     content = models.TextField()  # text content
-    price = models.DecimalField(max_digits=5, decimal_places=2)  # money -> Decimal allways | # decimal_field -> ragham ashar
+    price = models.DecimalField(max_digits=5, decimal_places=2)
     cover = models.ImageField(upload_to='covers/', blank=True)
 
     def __str__(self):
-        return self.title  # baraye neshan dadan dar panel admin
+        return self.title
 
     def get_absolute_url(self):
-        return reverse('book_detail', args=[self.id])  # baraye neshan dadan detail
-    # ke moaadele in khat code dar template hast {% url 'book_detail' book.id %}
+        return reverse('book_detail', args=[self.id])
+   
 
-
-"""
-* har taghiri dar database bayad dastore makemigrations va migrate zade shavad .
-makemigrations baraye tashkhis taghirat 
-migrate baraye zakhire taghirat dar database 
-"""
